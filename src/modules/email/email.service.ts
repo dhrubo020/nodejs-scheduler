b236ab9ba6b7feea3186e4d468fae1986d95e34e
Diptonil Singho <diptonil.singho@brainstation-23.com>
2024-04-28 23:31:00 +0600

    Added some comments

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -2,11 +2,17 @@ import * as schedule from 'node-schedule';
 import { getListOfBirthdayUser } from '../user/user.service';
 
 export function emailScheduler(cronSpec: string) {
-  schedule.scheduleJob(cronSpec, scheduleBirthdayWish);
+  try {
+    schedule.scheduleJob(cronSpec, scheduleBirthdayWish);
+  } catch (error) {
+    console.log('Error in initializing birthday email scheduler');
+  }
 }
 
 function scheduleBirthdayWish() {
+  // using batch process will be more efficient
   const userList = getListOfBirthdayUser();
+  // send in bulk will be better
   userList.map((e) => {
     const emailBody = getEmailBody(e.name);
     sendEmail(e.email, emailBody);
@@ -21,7 +27,12 @@ export function getEmailBody(username: string) {
 }
 
 export function sendEmail(toEmail: string, emailBody: string) {
-  // send email using email service
-  console.log('Sending email to', toEmail);
-  console.log(emailBody);
+  try {
+    // send email using email service
+    console.log('Sending email to', toEmail);
+    console.log(emailBody);
+  } catch (error) {
+    // save the email and retry later
+    console.log('Can not send email to', toEmail);
+  }
 }
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -7,7 +7,16 @@ router.get('/', (req, res) => {
   res.status(200).send({ sucess: true });
 });
 
+// get list of all user
 router.get('/customers', getUser);
+
+// register new user
+// req body exapmle:
+// {
+//   "name":"Jhon",
+//   "email":"jhon@mail.com",
+//   "birthday":"1990-05-02"
+// }
 router.post('/customers/register', register);
 
 export { router };
